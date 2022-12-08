6fcddedd7ba572a3a1f0cf07fb776e3ba8aebbb9
vupn <vupham140199@gmail.com>
2022-12-08 22:33:08 +0700

    complete

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,22 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+  static getDragons(...dragos) {
+    return dragos.map((drago) => {
+      return drago.name;
+    });
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+const toothless = new Dragon("Tootless", "Dark");
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,56 @@
 // Your code here
+const Dragon = require("./dragon");
+
+class EvilDragon extends Dragon {
+  constructor(name, color, evilDoings, nemesis) {
+    super(name, color);
+    this.evilDoings = evilDoings;
+    this.nemesis = nemesis;
+  }
+  dontInviteThemOverForDinner() {
+    let evilDoings = this.evilDoings;
+    evilDoings.forEach((evilDoing) => {
+      console.log(`${this.name} will ${evilDoing}`);
+    });
+  }
+  burnsNemesis() {
+    return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+  }
+}
+
+// const enderDragon = new EvilDragon(
+//   "Ender Dragon",
+//   "black",
+//   [
+//     "spit a fireball at Steve",
+//     "rule over all the Endermen",
+//     "destroy all blocks in its way",
+//   ],
+//   "Steve"
+// );
+// console.log(enderDragon);
+// enderDragon.dontInviteThemOverForDinner();
+// console.log(enderDragon.breathesFire());
+// console.log(enderDragon.burnsNemesis());
+//
+//
+// const drogon = new EvilDragon(
+//   "Drogon",
+//   "black-red",
+//   [
+//     "eat all your livestock",
+//     "burn down your castle in Westeros",
+//     "burn your father and brother"
+//   ],
+//   "Night King"
+// );
+// console.log(drogon);
+// drogon.dontInviteThemOverForDinner();
+// console.log(drogon.breathesFire());
+// console.log(drogon.burnsNemesis());
+
+try {
+  module.exports = EvilDragon;
+} catch {
+  module.exports = null;
+}
